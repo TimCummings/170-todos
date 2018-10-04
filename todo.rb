b7c93bc9462ec1bb82e83fbefb4e84030260d36f
Tim Cummings <timjc86@gmail.com>
2018-10-03 22:37:12 -0500

    lesson: creating a new todo list

--- todo.rb
+++ todo.rb
@@ -2,15 +2,32 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session['lists'] ||= []
+end
+
 get '/' do
   redirect '/lists'
 end
 
+# show all lists
 get '/lists' do
-  @lists = [
-    { name: 'Lunch Groceries', todos: [] },
-    { name: 'Dinner Groceries', todos: [] }
-  ]
-
+  @lists = session['lists']
   erb :lists
+end
+
+# render the new list form
+get '/lists/new' do
+  erb :new_list
+end
+
+# create a new list
+post '/lists' do
+  session['lists'] << { name: params[:list_name], todos: [] }
+  redirect '/lists'
 end
